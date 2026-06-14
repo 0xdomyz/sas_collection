ea6d92efb1e10c94e03377134e83b18c89ddbcc6
0xdomyz <yzdominic@outlook.com>
2026-06-14 22:53:01 +0100

    refactor(psi_fixed_simple_loop): streamline PSI query function parameters and output handling

--- proc_sql/psi/psi_fixed_simple_loop.py
+++ proc_sql/psi/psi_fixed_simple_loop.py
@@ -19,13 +19,8 @@ run;
 )
 
 # %%
-psi_in_table = "heart_time"
-psi_base_cls = f"age_decile between 0 and 9"
-psi_t_cls = f"age_decile = 10"
-psi_eps = 1e-6
 
-
-def make_fixed_simple_psi_qry(psi_cat_var, psi_in_table, psi_base_cls, psi_t_cls, psi_eps):
+def make_fixed_simple_psi_qry(psi_cat_var, in_table, base_cls, t_cls, out_table="work._psi_out_tbl_5", psi_eps = 1e-6):
 
     qry = f"""
 proc sql;
@@ -34,10 +29,10 @@ proc sql;
         {psi_cat_var},
         count(*) as n_base,
         calculated n_base / (
-            select count(*) from {psi_in_table} where {psi_base_cls}
+            select count(*) from {in_table} where {base_cls}
         ) as p_base
-    from {psi_in_table}
-    where {psi_base_cls}
+    from {in_table}
+    where {base_cls}
     group by {psi_cat_var};
 
     create table _t_dist_5 as
@@ -45,10 +40,10 @@ proc sql;
         {psi_cat_var},
         count(*) as n_t,
         calculated n_t / (
-            select count(*) from {psi_in_table} where {psi_t_cls}
+            select count(*) from {in_table} where {t_cls}
         ) as p_t
-    from {psi_in_table}
-    where {psi_t_cls}
+    from {in_table}
+    where {t_cls}
     group by {psi_cat_var};
 
     create table _psi_detail_5 as
@@ -64,22 +59,40 @@ proc sql;
     full outer join _t_dist_5 t
         on b.{psi_cat_var} = t.{psi_cat_var};
 
-    create table work._psi_out_tbl_5 as
+    create table {out_table} as
     select
         '{psi_cat_var}' as variable,
         sum(psi_component) as psi format=8.4
     from _psi_detail_5;
 quit;
 
-proc print data=work._psi_out_tbl_5;
-run;
+/*proc print data={out_table};*/
+/*run;*/
 """
     return qry
 
-
 # %%
-psi_vars = ["smoking_status", "bp_status"]
+sas.submitLST(f"proc datasets lib=work nolist; delete res_:; quit;", method="listandlog")
 
-for psi_cat_var in psi_vars:
-    qry = make_fixed_simple_psi_qry(psi_cat_var, psi_in_table, psi_base_cls, psi_t_cls, psi_eps)
+psi_vars = ["smoking_status", "bp_status"]
+in_table = "heart_time"
+base_cls = f"age_decile between 0 and 9"
+t_cls = f"age_decile = 10"
+
+for i, psi_cat_var in enumerate(psi_vars):
+    out_table = f"work.res_{i}"
+    qry = make_fixed_simple_psi_qry(psi_cat_var, in_table, base_cls, t_cls, out_table=out_table)
     sas.submitLST(qry, method="listonly")
+
+qry = f"""
+data work.res_final;
+    length
+        variable $50;
+    set work.res_:;
+run;
+
+proc print data=work.res_final;
+run;
+"""
+sas.submitLST(qry, method="listandlog")
+
