5410276988d9c22160864a8b2591fd949628ef81
0xdomyz <yzdominic@outlook.com>
2026-07-09 14:37:00 +0100

    feat(histograms): add additional histogram visualizations for MSRP and MPG

--- proc_stats/univariate/proc_univariate_charts.py
+++ proc_stats/univariate/proc_univariate_charts.py
@@ -30,6 +30,32 @@ run;
 """, method="listorlog")
 
 # %%
+# histogram with bin
+sas.submitLST(
+f"""
+proc sgplot data=sashelp.cars;
+    histogram msrp / nbins={20};
+    density msrp / type=normal;
+run;
+""", method="listorlog")
+
+# %%
+# 2-set histogram (overlay)
+sas.submitLST(
+f"""
+proc sgplot data=sashelp.cars;
+    histogram mpg_city / nbins={30} transparency=0.45 legendlabel="MPG City";
+    histogram mpg_highway / nbins={30} transparency=0.45 legendlabel="MPG Highway";
+    density mpg_city / type=normal legendlabel="City Normal";
+    density mpg_highway / type=normal legendlabel="Highway Normal";
+    keylegend / position=topright location=inside down=2;
+run;
+""",
+    method="listorlog",
+)
+
+
+# %%
 # qqplot
 sas.submitLST(
 f"""
@@ -58,6 +84,8 @@ proc univariate data=sashelp.cars noprint;
     probplot msrp / lognormal(shape=est);
 run;
 """, method="listorlog")
+
+# %%
 
 # %%
 
