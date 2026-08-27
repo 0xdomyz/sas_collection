0d6bd2f8916ad1487fb1311c0d26c31a03d3328c
0xdomyz <yzdominic@outlook.com>
2026-08-28 00:10:15 +0100

    misc

--- saspy_related/setup/test.py
+++ saspy_related/setup/test.py
@@ -4,4 +4,25 @@ import saspy
 sas = saspy.SASsession()
 
 # %%
-sas.sasdata("cars", "sashelp").head()
+qry = f"""
+proc sql;
+create table _tmp_qry as
+    select
+        *
+    from sashelp.cars a
+    ;
+quit;
+"""
+sas.submitLST(qry, method="listonly")
+df = sas.sasdata("_tmp_qry", "work").to_df()
+print(qry)
+df
+
+# %%
+import xlwings as xw
+
+ws = xw.sheets.active
+ws["A1"].value = df
+ws.tables.add(source=ws["A1"].expand())
+
+# %%
