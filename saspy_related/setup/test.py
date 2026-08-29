b9b5c9a76183208dd19c914fdc0cc1697fe3e1ba
0xdomyz <yzdominic@outlook.com>
2026-08-29 23:41:36 +0100

    misc

--- saspy_related/setup/test.py
+++ saspy_related/setup/test.py
@@ -19,10 +19,19 @@ print(qry)
 df
 
 # %%
+import seaborn as sns
+
+df = sns.load_dataset('titanic')
+print(f"{df.shape = }")
+print(df.head().to_string())
+
+# %%
 import xlwings as xw
 
+# xw.Book()
 ws = xw.sheets.active
+if ws["A1"].value is not None:
+    ws["A1"].expand().clear()
 ws["A1"].value = df
 ws.tables.add(source=ws["A1"].expand())
-
 # %%
